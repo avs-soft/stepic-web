5c08cb248caca57b02605d5246559d5df67f78b7
avs-soft <avs-soft@users.noreply.github.com>
2016-03-10 05:05:59 +1000

    for 2.3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='question_author_set')
+    likes = models.ManyToManyField(User, related_name='question_likes_set')
+    
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User) #, models.SET_NULL, null=True)
---
+++ init-mysql.sh
@@ -0,0 +1,8 @@
+sudo /etc/init.d/mysql restart
+#create db in mysql
+mysql -u root -e "CREATE DATABASE stepic_web_db;"
+mysql -u root -e "CREATE USER 'stepic_web'@'localhost' IDENTIFIED BY 'stepic_web_pass';"
+mysql -u root -e "GRANT ALL ON stepic_web_db.* TO 'stepic_web'@'localhost';"
+#???
+mysql -u root -e "FLUSH PRIVILEGES;"
+python ./ask/manage.py syncdb
