0049a2cffb34f44c0d2333c3fd6605b7c9c8a807
avs-soft <avs-soft@users.noreply.github.com>
2016-03-13 05:47:56 +1000

    added DROP DATABASE

--- init-mysql.sh
+++ init-mysql.sh
@@ -1,4 +1,5 @@
 sudo /etc/init.d/mysql restart
+mysql -u root -e "DROP DATABASE stepic_web_db;"
 #create db in mysql
 mysql -u root -e "CREATE DATABASE stepic_web_db;"
 mysql -u root -e "CREATE USER 'stepic_web'@'localhost' IDENTIFIED BY 'stepic_web_pass';"
